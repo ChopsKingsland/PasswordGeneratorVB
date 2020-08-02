49e5dc727dff00230211682274b9e544d84e8471
Chops Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2020-08-02 15:41:37 +0100

    Add files via upload

---
+++ pwdGeneratorUI/pwdGeneratorUI/main.vb
@@ -0,0 +1,37 @@
+﻿Public Class main
+
+
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim validchars As String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_!£$%^&*()[]{};:@#~/?.>,<"
+
+        Dim sb As New Text.StringBuilder()
+        Dim rand As New Random()
+        For i As Integer = 1 To TextBox2.Text
+            Dim idx As Integer = rand.Next(0, validchars.Length)
+            Dim randomChar As Char = validchars(idx)
+            sb.Append(randomChar)
+        Next i
+
+        Dim randomString = sb.ToString()
+
+        TextBox1.Text = (randomString)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        WindowState = WindowState.Minimized
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        End
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
